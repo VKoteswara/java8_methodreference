a07db2dc507ccc73e4b573d12cf6ad2402deb952
V Koteswara Rao Nilla <vnilla@nisum.com>
2020-02-24 19:01:08 +0530

    commit method reference project

---
+++ src/com/nisum/beans/Order.java
@@ -0,0 +1,38 @@
+package com.nisum.beans;
+
+public class Order {
+    Integer orderId;
+    String orderName;
+
+    public Order() {
+    }
+
+    public Order(Integer orderId, String orderName) {
+        this.orderId = orderId;
+        this.orderName = orderName;
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", orderName='" + orderName + '\'' +
+                '}';
+    }
+}
